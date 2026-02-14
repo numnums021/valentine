8435a562a8a01531315fab3dac1d3f36d9fda84d
fo.mind <59023791+numnums021@users.noreply.github.com>
2026-02-14 09:51:38 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Ты уверена?",
+    "Действительно уверена??",
+    "Ты уверена?",
+    "Нат...",
+    "Просто подумай об этом!",
+    "Если ты скажешь нет, мне будет очень грустно...",
+    "Мне будет очень грустно...",
+    "Мне будет очень, очень, очень грустно...",
+    "Ладно, я перестану спрашивать...",
+    "Шучу, скажи да! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
